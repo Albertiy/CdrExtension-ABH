b33d7dc309be10f1ea534773302d016749ffaf3c
Albert/袁悦 <YUANYUE963@outlook.com>
2021-05-13 14:45:03 +0800

    [2021-05-13 14:43:09] 添加WPF安装器项目

---
+++ Installer/ABHInstaller/ABHInstaller/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ABHInstaller
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
